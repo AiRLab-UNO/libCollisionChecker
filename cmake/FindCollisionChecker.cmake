b576b460ca4741ac24f75f62b3936281d085f2b6
Redwan Newaz <redwan06me@gmail.com>
2026-01-24 09:28:14 -0600

    bug fixed VAMP added

--- cmake/FindCollisionChecker.cmake
+++ cmake/FindCollisionChecker.cmake
@@ -1,144 +1,139 @@
 # FindCollisionChecker.cmake
-# Finds collision checker libraries built from libcollisionchecker project
 #
-# This module defines:
-#  CollisionChecker_FOUND - True if at least one collision checker library is found
-#  CollisionChecker_LIBRARIES - List of all found collision checker libraries
-#  CollisionChecker_INCLUDE_DIRS - List of all include directories
-#  CollisionChecker_<COMPONENT>_FOUND - True if specific component is found
-#  CollisionChecker_<COMPONENT>_LIBRARY - Path to specific component library
-#  CollisionChecker_<COMPONENT>_INCLUDE_DIR - Include directory for specific component
+# Provides:
+#   CollisionChecker_FOUND
+#   CollisionChecker_INCLUDE_DIRS
+#   CollisionChecker_LIBRARIES
 #
-# Components: OctreeCollisionChecker, QuadtreeCollisionChecker, OccupancyMap, FCL, VampCollisionChecker
-#
-# Usage:
-#  find_package(CollisionChecker REQUIRED COMPONENTS OctreeCollisionChecker QuadtreeCollisionChecker)
-#  or
-#  find_package(CollisionChecker REQUIRED) # finds all available libraries
+# Imported targets:
+#   CollisionChecker::OccupancyMap
+#   CollisionChecker::Octree
+#   CollisionChecker::Quadtree
+#   CollisionChecker::Vamp
 
-# Define the base search path (you may need to adjust this)
-set(CollisionChecker_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../libcollisionchecker" CACHE PATH "Root directory of libcollisionchecker")
-
-# Initialize variables
-set(CollisionChecker_FOUND FALSE)
-set(CollisionChecker_LIBRARIES)
-set(CollisionChecker_INCLUDE_DIRS)
-
-# Define available components
-set(CollisionChecker_COMPONENTS
-        OctreeCollisionChecker
-        QuadtreeCollisionChecker
-        OccupancyMap
-        FCL
-        VampCollisionChecker
-)
-
-# Component to subdirectory mapping for includes
-set(OctreeCollisionChecker_SUBDIR "octree")
-set(QuadtreeCollisionChecker_SUBDIR "quadtree")
-set(OccupancyMap_SUBDIR "occupancy")
-set(FCL_SUBDIR "FCL")
-set(VampCollisionChecker_SUBDIR "VAMP")
-
-# If no components specified, try to find all
-if(NOT CollisionChecker_FIND_COMPONENTS)
-    set(CollisionChecker_FIND_COMPONENTS ${CollisionChecker_COMPONENTS})
+if(NOT CollisionChecker_ROOT)
+    set(CollisionChecker_ROOT "${CMAKE_SOURCE_DIR}/libCollisionChecker" CACHE PATH "Root directory of CollisionChecker")
 endif()
-
-# Search for each requested component
-foreach(component ${CollisionChecker_FIND_COMPONENTS})
-    # Check if component is valid
-    list(FIND CollisionChecker_COMPONENTS ${component} component_index)
-    if(component_index EQUAL -1)
-        if(CollisionChecker_FIND_REQUIRED_${component})
-            message(FATAL_ERROR "Unknown CollisionChecker component: ${component}")
-        else()
-            message(WARNING "Unknown CollisionChecker component: ${component}")
-        endif()
-        continue()
-    endif()
-
-    # Find the library
-    find_library(CollisionChecker_${component}_LIBRARY
-            NAMES ${component}
-            HINTS
-            ${CollisionChecker_ROOT_DIR}/build
-            ${CollisionChecker_ROOT_DIR}/lib
-            ${CMAKE_INSTALL_PREFIX}/lib
-            PATH_SUFFIXES
-            lib
-    )
-
-    # Find the include directory
-    set(component_subdir ${${component}_SUBDIR})
-    find_path(CollisionChecker_${component}_INCLUDE_DIR
-            NAMES ${component}.h ${component}.hpp
-            HINTS
-            ${CollisionChecker_ROOT_DIR}/include/${component_subdir}
-            ${CollisionChecker_ROOT_DIR}/include
-            ${CMAKE_INSTALL_PREFIX}/include
-            PATH_SUFFIXES
-            ${component_subdir}
-            include/${component_subdir}
-    )
-
-    # Handle REQUIRED flag for this component
-    if(CollisionChecker_${component}_LIBRARY AND CollisionChecker_${component}_INCLUDE_DIR)
-        set(CollisionChecker_${component}_FOUND TRUE)
-        list(APPEND CollisionChecker_LIBRARIES ${CollisionChecker_${component}_LIBRARY})
-        list(APPEND CollisionChecker_INCLUDE_DIRS ${CollisionChecker_${component}_INCLUDE_DIR})
-
-        # Also add the parent include directory
-        get_filename_component(parent_include_dir ${CollisionChecker_${component}_INCLUDE_DIR} DIRECTORY)
-        list(APPEND CollisionChecker_INCLUDE_DIRS ${parent_include_dir})
-
-        if(NOT CollisionChecker_FIND_QUIETLY)
-            message(STATUS "Found ${component}: ${CollisionChecker_${component}_LIBRARY}")
-        endif()
-    else()
-        set(CollisionChecker_${component}_FOUND FALSE)
-        if(CollisionChecker_FIND_REQUIRED_${component})
-            message(FATAL_ERROR "Could not find required CollisionChecker component: ${component}")
-        elseif(NOT CollisionChecker_FIND_QUIETLY)
-            message(STATUS "Could not find CollisionChecker component: ${component}")
-        endif()
-    endif()
-endforeach()
-
-# Remove duplicate include directories
-if(CollisionChecker_INCLUDE_DIRS)
-    list(REMOVE_DUPLICATES CollisionChecker_INCLUDE_DIRS)
-endif()
-
-# Set the overall FOUND flag
-if(CollisionChecker_LIBRARIES)
-    set(CollisionChecker_FOUND TRUE)
-endif()
-
-# Handle standard arguments
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CollisionChecker
-        REQUIRED_VARS CollisionChecker_LIBRARIES CollisionChecker_INCLUDE_DIRS
-        HANDLE_COMPONENTS
+
+# ---------------------------------------------------------
+# Find include directory
+# ---------------------------------------------------------
+find_path(CollisionChecker_INCLUDE_DIR
+        NAMES CollisionChecker.hh
+        PATH_SUFFIXES
+        include
+        include/collision
+        HINTS ${CollisionChecker_ROOT}
+        NO_DEFAULT_PATH # Ensures it looks in your specific lib folder first
 )
 
-# Mark variables as advanced
-mark_as_advanced(
-        CollisionChecker_ROOT_DIR
-        CollisionChecker_LIBRARIES
-        CollisionChecker_INCLUDE_DIRS
+# ---------------------------------------------------------
+# Find libraries
+# ---------------------------------------------------------
+#We use HINTS and PATH_SUFFIXES instead of link_directories
+set(_CC_LIB_SUFFIXES lib bin)
+find_library(CollisionChecker_OCCUPANCY_LIB
+        NAMES OccupancyMap
+        HINTS ${CollisionChecker_ROOT}
+        PATH_SUFFIXES ${_CC_LIB_SUFFIXES}
 )
 
-foreach(component ${CollisionChecker_COMPONENTS})
-    mark_as_advanced(
-            CollisionChecker_${component}_LIBRARY
-            CollisionChecker_${component}_INCLUDE_DIR
+find_library(CollisionChecker_OCTREE_LIB
+        NAMES OctreeCollisionChecker
+        HINTS ${CollisionChecker_ROOT}
+        PATH_SUFFIXES ${_CC_LIB_SUFFIXES}
+)
+
+find_library(CollisionChecker_QUADTREE_LIB
+        NAMES QuadtreeCollisionChecker
+        HINTS ${CollisionChecker_ROOT}
+        PATH_SUFFIXES ${_CC_LIB_SUFFIXES}
+)
+
+find_library(CollisionChecker_VAMP_LIB
+        NAMES VampCollisionChecker
+        HINTS ${CollisionChecker_ROOT}
+        PATH_SUFFIXES ${_CC_LIB_SUFFIXES}
+)
+
+
+find_library(CollisionChecker_FCL_LIB
+        NAMES FCL
+        HINTS ${CollisionChecker_ROOT}
+        PATH_SUFFIXES ${_CC_LIB_SUFFIXES}
+)
+
+# ---------------------------------------------------------
+# Standard package handling
+# ---------------------------------------------------------
+find_package_handle_standard_args(
+        CollisionChecker
+        REQUIRED_VARS
+        CollisionChecker_INCLUDE_DIR
+        CollisionChecker_OCCUPANCY_LIB
+        CollisionChecker_OCTREE_LIB
+        CollisionChecker_QUADTREE_LIB
+        CollisionChecker_VAMP_LIB
+        CollisionChecker_FCL_LIB
+)
+
+# ---------------------------------------------------------
+# Export variables
+# ---------------------------------------------------------
+if (CollisionChecker_FOUND)
+    set(CollisionChecker_INCLUDE_DIRS
+            ${CollisionChecker_INCLUDE_DIR}
     )
-endforeach()
 
-# Print summary if not quiet
-if(CollisionChecker_FOUND AND NOT CollisionChecker_FIND_QUIETLY)
-    message(STATUS "CollisionChecker found:")
-    message(STATUS "  Libraries: ${CollisionChecker_LIBRARIES}")
-    message(STATUS "  Include dirs: ${CollisionChecker_INCLUDE_DIRS}")
+    set(CollisionChecker_LIBRARIES
+            ${CollisionChecker_OCCUPANCY_LIB}
+            ${CollisionChecker_OCTREE_LIB}
+            ${CollisionChecker_QUADTREE_LIB}
+            ${CollisionChecker_VAMP_LIB}
+            ${CollisionChecker_FCL_LIB}
+    )
+
+    # -----------------------------------------------------
+    # Imported targets (modern CMake)
+    # -----------------------------------------------------
+    if (NOT TARGET CollisionChecker::OccupancyMap)
+        add_library(CollisionChecker::OccupancyMap SHARED IMPORTED)
+        set_target_properties(CollisionChecker::OccupancyMap PROPERTIES
+                IMPORTED_LOCATION ${CollisionChecker_OCCUPANCY_LIB}
+                INTERFACE_INCLUDE_DIRECTORIES ${CollisionChecker_INCLUDE_DIR}
+        )
+    endif()
+
+    if (NOT TARGET CollisionChecker::FCL)
+        add_library(CollisionChecker::FCL SHARED IMPORTED)
+        set_target_properties(CollisionChecker::FCL PROPERTIES
+                IMPORTED_LOCATION ${CollisionChecker_FCL_LIB}
+                INTERFACE_INCLUDE_DIRECTORIES ${CollisionChecker_INCLUDE_DIR}
+        )
+    endif()
+
+    if (NOT TARGET CollisionChecker::Octree)
+        add_library(CollisionChecker::Octree SHARED IMPORTED)
+        set_target_properties(CollisionChecker::Octree PROPERTIES
+                IMPORTED_LOCATION ${CollisionChecker_OCTREE_LIB}
+                INTERFACE_INCLUDE_DIRECTORIES ${CollisionChecker_INCLUDE_DIR}
+        )
+    endif()
+
+    if (NOT TARGET CollisionChecker::Quadtree)
+        add_library(CollisionChecker::Quadtree SHARED IMPORTED)
+        set_target_properties(CollisionChecker::Quadtree PROPERTIES
+                IMPORTED_LOCATION ${CollisionChecker_QUADTREE_LIB}
+                INTERFACE_INCLUDE_DIRECTORIES ${CollisionChecker_INCLUDE_DIR}
+        )
+    endif()
+
+    if (NOT TARGET CollisionChecker::Vamp)
+        add_library(CollisionChecker::Vamp SHARED IMPORTED)
+        set_target_properties(CollisionChecker::Vamp PROPERTIES
+                IMPORTED_LOCATION ${CollisionChecker_VAMP_LIB}
+                INTERFACE_INCLUDE_DIRECTORIES ${CollisionChecker_INCLUDE_DIR}
+        )
+    endif()
 endif()
